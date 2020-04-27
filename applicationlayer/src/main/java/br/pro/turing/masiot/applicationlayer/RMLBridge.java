d11779e53f9f7cefc9d86f09e124dfb85fc0188e
Fabian <fabiancpbm@gmail.com>
2020-04-27 09:04:50 -0300

    [#7] - Application layer javadoc.

--- applicationlayer/src/main/java/br/pro/turing/masiot/applicationlayer/RMLBridge.java
+++ applicationlayer/src/main/java/br/pro/turing/masiot/applicationlayer/RMLBridge.java
@@ -18,17 +18,31 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * The bridge of the application layer between the cliente and the RML.
+ */
 public class RMLBridge implements NodeConnectionListener {
 
-    private static final Logger LOGGER = LoggerUtils.initLogger(RMLBridge.class.getClassLoader().getResourceAsStream(
-            "br/pro/turing/masiot/applicationlayer/applicationlayer.logging.properties"), RMLBridge.class.getSimpleName());
+    /** Logger. */
+    private static final Logger LOGGER = LoggerUtils.initLogger(RMLBridge.class.getClassLoader()
+                    .getResourceAsStream("br/pro/turing/masiot/applicationlayer/applicationlayer.logging.properties"),
+            RMLBridge.class.getSimpleName());
 
+    /** IoT by ContextNet connection instance. */
     private MrUdpNodeConnection connection;
 
+    /** IP socket address of the ContextNet gateway. */
     private InetSocketAddress gatewayAddress;
 
+    /** Connection state of the Application layer. */
     private ConnectionState connectionState = ConnectionState.OFFLINE;
 
+    /**
+     * Connects to the RML.
+     *
+     * @param gatewayIP   Gateway IP.
+     * @param gatewayPort Gateway port.
+     */
     public void connect(String gatewayIP, int gatewayPort) {
         LOGGER.info("Connecting this client to RML.");
         this.gatewayAddress = new InetSocketAddress(gatewayIP, gatewayPort);
@@ -41,6 +55,12 @@ public class RMLBridge implements NodeConnectionListener {
         }
     }
 
+    /**
+     * Create an action in a instant of time to perform a command.
+     *
+     * @param localDateTime Instant time.
+     * @param command       Command to be performed.
+     */
     private void createAction(LocalDateTime localDateTime, Command command) {
         Action action = new Action(localDateTime, command.get_id());
         ApplicationMessage message = new ApplicationMessage();
@@ -53,29 +73,49 @@ public class RMLBridge implements NodeConnectionListener {
         }
     }
 
-    @Override
-    public void newMessageReceived(NodeConnection nodeConnection, Message message) {
-
-    }
-
+    /**
+     * Informs that this Application layer is connected to RML server.
+     *
+     * @param nodeConnection Node connection.
+     */
     @Override
     public void connected(NodeConnection nodeConnection) {
         LOGGER.info("Client connected.");
         this.connectionState = ConnectionState.ONLINE;
     }
 
+    /**
+     * Informs that this Application layer is reconnected to TML server.
+     *
+     * @param nodeConnection Node connection.
+     * @param socketAddress  Socket address.
+     * @param b              B
+     * @param b1             B1
+     */
     @Override
     public void reconnected(NodeConnection nodeConnection, SocketAddress socketAddress, boolean b, boolean b1) {
         LOGGER.info("Client reconnected.");
         this.connectionState = ConnectionState.ONLINE;
     }
 
+    /**
+     * Informs that this Application layer is disconnected from RML server.
+     *
+     * @param nodeConnection Node connection.
+     */
     @Override
     public void disconnected(NodeConnection nodeConnection) {
         LOGGER.info("Client disconnected.");
         this.connectionState = ConnectionState.OFFLINE;
     }
 
+    /**
+     * Informs that some messages was not sent. If this Application layer is connected to RML server, the unsert
+     * messages will be resent.
+     *
+     * @param nodeConnection Node connection.
+     * @param list           Unsent messages list.
+     */
     @Override
     public void unsentMessages(NodeConnection nodeConnection, List<Message> list) {
         StringBuilder errorMessageLog = new StringBuilder();
@@ -94,6 +134,11 @@ public class RMLBridge implements NodeConnectionListener {
                 }
             }
         }
+    }
+
+    @Override
+    public void newMessageReceived(NodeConnection nodeConnection, Message message) {
+
     }
 
     @Override
